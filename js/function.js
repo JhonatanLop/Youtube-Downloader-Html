1caf835bd48a1b4a3549be7840f694d1a29164dd
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-01-31 20:06:40 -0300

    bug fix alteração do caminho endpoint

--- js/function.js
+++ js/function.js
@@ -11,19 +11,19 @@ const typeInput = document.getElementsByName('type');
 
 // Adiciona um evento de clique ao botão
 botao_pesq.addEventListener('click', () => {
-// Obtém o valor do input de link
-const link = linkInput.value;
-// Separa o ID do vídeo usando o método split
-const videoId = link.split('v=')[1];
-// Define a chave API
-const apiKey = 'YOUR_API_KEY'
-// Constrói a URL da API do YouTube com o ID do vídeo e a chave API
-const url = https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey};
+  // Obtém o valor do input de link
+  const link = linkInput.value;
+  // Separa o ID do vídeo usando o método split
+  const videoId = link.split('v=')[1];
+  // Define a chave API
+  const apiKey = 'YOUR_API_KEY'
+  // Constrói a URL da API do YouTube com o ID do vídeo e a chave API
+  const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`;
 
-// Faz uma chamada à API usando o método fetch
-fetch(url)
-// Transforma a resposta em JSON
-.then(response => response.json())
+  // Faz uma chamada à API usando o método fetch
+  fetch(url)
+    // Transforma a resposta em JSON
+    .then(response => response.json())
     // Obtém o título do vídeo e atualiza o valor do input de título
     .then(data => {
       const titulo = data.items[0].snippet.title;
@@ -31,51 +31,34 @@ fetch(url)
     });
 });
 
+// adiciona a função downloadVideo ao evento de clique do botão de download
+botao_download.addEventListener("click", downloadVideo);
+
 // parte download
-// adiciona o evendo do botão de download
-botao_download.addEventListener("click", async function(){
-  // pega o valor do input de link
-  const link = linkInput.value;
-  // pega o valor do tipo de download
-  let type = "";
-  for (let i = 0; i < typeInput.length; i++) {
-    if (typeInput[i].checked) {
-        type = typeInput[i].value;
-        break;
+// cria a função downloadVideo
+async function downloadVideo() {
+    // pega o valor do input de link
+    const link = linkInput.value;
+    // pega o valor do radio button selecionado
+    const type = document.querySelector('input[name="type"]:checked').value;
+    
+    // cria um objeto FormData para armazenar os dados
+    const formData = new FormData();
+    // adiciona o link e o type ao objeto FormData
+    formData.append("link", link);
+    formData.append("type", type);
+
+    // faz uma solicitação POST para o servidor
+    const response = await fetch("http://localhost:5500/python/download", {
+      method: "POST",
+      body: formData
+    });
+
+    if (response.status === 200) {
+      // pega o texto da resposta e exibe-o
+      const responseText = await response.text();
+      console.log(responseText);
+    } else {
+      alert("Ocorreu um erro ao fazer o download, por favor tente novamente.");
     }
-  }
-  // cria um objeto FormData para armazenar os dados
-  const formData = new FormData();
-  // adiciona o link e o tipo ao objeto FormData
-  formData.append("link", link);
-  formData.append("type", type);
-
-  // faz uma solicitação POST para o servidor
-  const response = await fetch("http://localhost:5500/download", {
-     botao_download.addEventListener("click", async function(){
-  // pega o valor do input de link
-  const link = linkInput.value;
-  // pega o valor do radio button selecionado
-  const type = document.querySelector('input[name="type"]:checked').value;
-
-  // cria um objeto FormData para armazenar os dados
-  const formData = new FormData();
-  // adiciona o link e o type ao objeto FormData
-  formData.append("link", link);
-  formData.append("type", type);
-
-// faz uma solicitação POST para o servidor
-const response = await fetch("http://localhost:5500/download", {
-  method: "POST",
-  body: formData
-  });
-
-if (response.status === 200) {
-// pega o texto da resposta e exibe-o
-  const responseText = await response.text();
-  console.log(responseText);
-  } 
-  else {
-  alert("Ocorreu um erro ao fazer o download, por favor tente novamente mais tarde.");
-        }
-  });
+}
--- python/function.py
+++ python/function.py
@@ -8,21 +8,23 @@ app = Flask(__name__)
 CORS(app)
 
 # define o endpoint '/download' com o método POST
-@app.route('/download', methods=['POST'])
-def download():
+@app.route('/download/python', methods=['POST'])
+def download(link):
     # pega os valores do formulário enviado pelo javascript
     link = request.form['link']
     type = request.form['type']
     try:
         # cria um objeto YouTube com o link
         video = YouTube(link)
-        # verifica a resolução escolhida e seleciona o stream correspondente
+        # pega a melhor resolução possível
         video = video.streams.get_highest_resolution()
         # verifica o tipo escolhido e filtra somente o audio, caso seja escolhido
         if type=='audio':
             video = video.filter(only_audio=True)
+            video.download()
+        else:
+            video.download()
         # faz o download do video
-        video.download()
         return "Download concluído com sucesso!"
     except Exception as e:
         # caso ocorra algum erro, retorna uma mensagem de erro
